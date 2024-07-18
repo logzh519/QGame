4898505f7dcc2413dc91c519bcf47c74f5f18957
张彬强 <316455@nd.com>
2024-07-18 09:23:34 +0800

    添加PlayInput控制角色状态动画测试；

---
+++ Project/Assets/Assets/Input/CharacterController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController : MonoBehaviour
+{
+    private Animator _animator;
+    private float _threshold = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void PlayerMove(InputAction.CallbackContext callbackContext)
+    {
+        Vector2 movement = callbackContext.ReadValue<Vector2>();
+        Debug.Log(movement.ToString());
+
+        if (movement.y > _threshold)
+        {
+            _animator.SetBool("Walk", true);
+        }
+        else if(movement.y <= _threshold)
+        {
+            _animator.SetBool("Walk", false);
+        }
+        
+        if (movement.y < -_threshold)
+        {
+            _animator.SetBool("Walk Backward", true);
+        }
+        else if(movement.y >= _threshold)
+        {
+            _animator.SetBool("Walk Backward", false);
+        }
+        
+        
+    }
+}
