9e6fc29757fe0c362e02d77b3b54d6914ab1bdce
张彬强 <316455@nd.com>
2024-07-18 10:37:41 +0800

    移除角色的Root Motion选项，优化动画前进后退控制

--- Project/Assets/Assets/Input/CharacterController.cs
+++ Project/Assets/Assets/Input/CharacterController.cs
@@ -1,10 +1,19 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
+    public float forwardSpeed = 2.0f;
+    public float backwardSpeed = 1.5f;
+    private Vector3 _movement;
+    private float _targetSpeed = 0.0f;
+    private float _currentSpeed = 0.0f;
+
     private Animator _animator;
-    private float _threshold = 0.1f;
+    private readonly float _threshold = 0.1f;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -12,13 +21,29 @@ public class CharacterController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        Move();
+    }
+
+
+    void Move()
+    {
+        _currentSpeed  = Mathf.Lerp(_targetSpeed, _currentSpeed, 0.9f);
+        _movement = new Vector3(0, 0, _currentSpeed * Time.deltaTime);
+        transform.position += _movement;
+    }
+    
+
     public void PlayerMove(InputAction.CallbackContext callbackContext)
     {
+        _targetSpeed = 0;
         Vector2 movement = callbackContext.ReadValue<Vector2>();
         Debug.Log(movement.ToString());
 
         if (movement.y > _threshold)
         {
+            _targetSpeed = forwardSpeed * movement.y;
             _animator.SetBool("Walk", true);
         }
         else if(movement.y <= _threshold)
@@ -28,13 +53,12 @@ public class CharacterController : MonoBehaviour
         
         if (movement.y < -_threshold)
         {
+            _targetSpeed = backwardSpeed * movement.y;
             _animator.SetBool("Walk Backward", true);
         }
         else if(movement.y >= -_threshold)
         {
             _animator.SetBool("Walk Backward", false);
         }
-        
-        
     }
 }
